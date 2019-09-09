1a57c5ead63dab366045db654c7fbf559b63d48b
eric2788 <tsukiko852@gmail.com>
2019-09-09 13:29:36 +0800

    added new game utils

--- API/src/com/ericlam/mc/minigames/core/manager/GameUtils.java
+++ API/src/com/ericlam/mc/minigames/core/manager/GameUtils.java
@@ -1,6 +1,12 @@
 package com.ericlam.mc.minigames.core.manager;
 
+import com.ericlam.mc.minigames.core.character.GamePlayer;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
 
 public interface GameUtils {
 
@@ -9,4 +15,10 @@ public interface GameUtils {
     String getTimer(long sec);
 
     void playSound(Player player, String[] soundString);
+
+    <T> void unLagIterate(Collection<T> collection, Consumer<T> task, long period);
+
+    void noLagTeleport(List<GamePlayer> gamePlayers, List<Location> locations, long period);
+
+    void noLagTeleport(List<GamePlayer> gamePlayers, long period, Location... locations);
 }
